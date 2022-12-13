f53e1f42c5ae3aa6241b4b05a3f6341c42e87b39
yotorogo <112503800+yotorogo@users.noreply.github.com>
2022-12-12 22:21:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-
-
 const d = new Date();
 document.getElementById("date").innerHTML = d;
 
 let day = d.getDay();
 //let day = 0;
 document.getElementById("day").innerHTML = day;
-
-const pics = ["https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sunday-quotes-index-1585055167.jpg", "https://www.thefactsite.com/wp-content/uploads/2017/10/monday-facts.jpg", "http://justfunfacts.com/wp-content/uploads/2021/11/tuesday-1.jpg", "https://miro.medium.com/max/800/0*IwtASl5bnsqceOdT.jpg", "https://cdn1.vectorstock.com/i/1000x1000/52/70/icon-page-calendar-day-week-thursday-vector-37535270.jpg", "https://goodlucksymbols.com/wp-content/uploads/2020/09/Friday-meaning-superstitions.jpg", "https://svinews.com/wp-content/uploads/2021/09/Saturday.png"];
 
 document.getElementById("calendar_image").innerHTML = '<img src="' + pics[day] +'">';
 
@@ -28,24 +24,24 @@ function myFunction (){
 
 	}
 	if(day == 0){
-	document.getElementById("calendar_image").innerHTML = '<img src="' + pics[0] +'">';
+	document.getElementById("calendar_image").innerHTML = '<img src="./images/Monday.jpeg' + pics[0] +'">';
 }
 else if(day == 1){
-   document.getElementById("calendar_image").innerHTML = '<img src="' + pics[1] +'">';
+   document.getElementById("calendar_image").innerHTML = '<img src="./images/Tuesday.jpeg' + pics[1] +'">';
 }
 else if(day == 2){
-   document.getElementById("calendar_image").innerHTML = '<img src="' + pics[2] +'">';
+   document.getElementById("calendar_image").innerHTML = '<img src="./images/Wednesday.jpeg' + pics[2] +'">';
 }
 else if(day == 3){
-   document.getElementById("calendar_image").innerHTML = '<img src="' + pics[3] +'">';
+   document.getElementById("calendar_image").innerHTML = '<img src="./images/Thursday.jpeg' + pics[3] +'">';
 }
 else if(day == 4){
-   document.getElementById("calendar_image").innerHTML = '<img src="' + pics[4] +'">';
+   document.getElementById("calendar_image").innerHTML = '<img src="./images/Friday.jpeg' + pics[4] +'">';
 }
 else if(day == 5){
-   document.getElementById("calendar_image").innerHTML = '<img src="' + pics[5] +'">';
+   document.getElementById("calendar_image").innerHTML = '<img src="./images/Saturday.jpeg' + pics[5] +'">';
 }
 else if(day == 6){
-   document.getElementById("calendar_image").innerHTML = '<img src="' + pics[6] +'">';
+   document.getElementById("calendar_image").innerHTML = '<img src="./images/Sunday.jpeg' + pics[6] +'">';
 }
 }
